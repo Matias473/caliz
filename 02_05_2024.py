e1c571ace360ac0a0764bd4448c4fc3759cde6e4
Matias Rosales <145402946+Matias473@users.noreply.github.com>
2024-06-10 12:09:30 -0600

    jaladas

---
+++ 02_05_2024.py
@@ -0,0 +1,31 @@
+#vamos a empezar desde cero
+#hola mundo
+print("hola mundo")
+
+#comentario en varias lineas
+"""
+comentario en varias lineas, 
+se crea usando 3 comillas arriba y abajo
+c:
+"""
+
+#variables
+"""
+es de bueno recordar que las variables deben ser:
+minusculas
+separadas con (_)
+EJEMPLO
+my_firts_date
+food
+number
+_if 
+"""
+my_name = "matias rosales"
+print(my_name)
+_if = "hello" #random word
+print(_if)
+years_old = "19"
+print(years_old)
+
+#juntar datos
+print(_if, my_name)
